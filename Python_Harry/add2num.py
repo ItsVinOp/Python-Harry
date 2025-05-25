73d02ad417532bfb5551bde9660130dcdf504491
ItsVinOp <117188366+ItsVinOp@users.noreply.github.com>
2025-05-25 22:50:56 +0530

    Add files via upload

---
+++ Python_Harry/add2num.py
@@ -0,0 +1,12 @@
+# x = int(input("Enter a 1st number: "))
+# Y = int(input(" Enter a 2nd number: "))
+
+# print(x + Y)
+
+# # a = 12
+
+a = input("Enter a 1st number: ") # input() returns a string
+a = int(a)
+b = input("Enter a 1st number: ") # input() returns a string
+b =int(b)
+print(a + b)
