340a14f92eaca6f37ecb867f16d1f8814fe05f46
ItsVinOp <vinayakshirke97206@gmail.com>
2025-05-27 12:04:16 +0530

    Commit

---
+++ Python_Harry/sep&end.py
@@ -0,0 +1,4 @@
+print("hello world")
+
+print("how are you", 5, 6,sep="/")
+print("I am fine", 7, "8")
---
+++ operator.py
@@ -0,0 +1,17 @@
+# a= 12
+# b = 3
+
+# print("a - b =", a - b)
+# print("a + b =", a + b)
+# print("a / b =", a / b)
+# print("a % b =", a % b)
+
+
+# conditional operators
+
+a = 10
+b = 5
+print (a>b)
+print (a<b)
+print (a==b)
+print (a!=10)
