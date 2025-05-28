ec3b18ddc1a123f9b4cfd7d3410ff479cfefc5fe
ItsVinOp <vinayakshirke97206@gmail.com>
2025-05-28 16:10:46 +0530

    string

---
+++ Python_Harry/slicing.py
@@ -0,0 +1,10 @@
+name = "VinayakShirke"
+
+# print(name[0:3])
+# print(name[2:7]) #2 se start and n-1 end
+# print(name[2:-0]) 
+
+print(name[0:10:3])  #3-1=2 skip 
+
+print(name[:13])
+print(name[1:])
---
+++ Python_Harry/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(name[0:3])
+# print(name[2:7]) #2 se start and n-1 end
+# print(name[2:-0]) 
