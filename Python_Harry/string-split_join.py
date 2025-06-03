ee96d03444e3ac8016b7bf8d936ac63ca618d8d0
ItsVinOp <vinayakshirke97206@gmail.com>
2025-06-03 15:01:38 +0530

    String-split&join property

---
+++ Python_Harry/string-split_join.py
@@ -0,0 +1,17 @@
+text = "apples, oranges, bananas"
+
+print(text.split(", "))  
+
+
+print(",".join(["apple", "orang", "bananas"]))
+
+
+# //check property
+
+text = "Python124"
+
+print(text.isalnum())
+print(text.isalpha())
+print(text.isdigit())
+print(text.islower())
+print(text.isupper())
