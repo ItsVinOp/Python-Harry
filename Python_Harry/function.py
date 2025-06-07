7a0d652300a4046489ccb4d4b97ad0403469f9ef
ItsVinOp <vinayakshirke97206@gmail.com>
2025-06-07 13:56:12 +0530

    function

---
+++ Python_Harry/function.py
@@ -0,0 +1,17 @@
+a = 10
+b = 20
+c = 3
+
+average = (a + b + c) / 3
+# print(average)
+
+def average(a,b,c):
+  d = (a + b + c) /3
+  # print (d)
+  return d
+
+average(10, 20, 30)
+average(10, 20, 2)
+
+O1 = average(10, 20, 30)
+print(O1)
