d4b124930380548dcacbffb4c3f079bc19d558f8
ItsVinOp <vinayakshirke97206@gmail.com>
2025-06-07 18:27:37 +0530

    lambda function

---
+++ Python_Harry/lambda.py
@@ -0,0 +1,13 @@
+# one liner function
+# pass func to func
+
+# def square(x):
+#     return x*x
+# def sum(x,y):
+#     return x+y
+
+square = lambda x: x*x
+sum = lambda x,y: x+y
+
+print(square(7))
+print(sum(10,3))
